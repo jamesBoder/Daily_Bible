3af41f6aecbe0adb7b2523ce8598e764b1b4d741
jamesboder <james.macean08@gmail.com>
2026-01-16 21:43:01 -0500

    Update app.tsx to use BrowserRouter & AuthProvider

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,20 @@
 import React from "react";
-
+import { BrowserRouter as Router } from "react-router-dom";
+import { AuthProvider } from "./contexts/AuthContext";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App App-header">
-      <h1>Words of Praise app</h1>
-      <p>Coming Soon</p>
-    </div>
+    <Router>
+      <AuthProvider>
+        <div className="App App-header">
+          <h1 className="text-3xl font-bold text-primary-600">
+            Words of Praise app
+          </h1>
+          <p className="text-gray-600 mt-4">Coming Soon</p>
+        </div>
+      </AuthProvider>
+    </Router>
   );
 }
 
