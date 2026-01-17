a40bb66ae42aa0bb07436d596e1272fb37060595
jamesboder <james.macean08@gmail.com>
2026-01-16 21:38:30 -0500

    Create useAuth hook

--- frontend/src/hooks/useAuth.ts
+++ frontend/src/hooks/useAuth.ts
@@ -1,1 +1,12 @@
-export {};
+import { useContext } from 'react';
+import { AuthContext } from '../contexts/AuthContext';
+
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+  
+  if (context === undefined) {
+    throw new Error('useAuth must be used within an AuthProvider');
+  }
+  
+  return context;
+};
