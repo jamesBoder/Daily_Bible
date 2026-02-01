77d8a2e0b4546ae2dd23005a6222a0d502856455
jamesboder <james.macean08@gmail.com>
2026-02-01 00:42:46 -0500

    removed tanstack icon (development mode)

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import { reportWebVitals } from "./reportWebVitals";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./lib/queryClient";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { ErrorBoundary } from "./components/common/ErrorBoundary";
 import "./index.css";
 import App from "./App";
@@ -18,7 +17,6 @@ root.render(
     <ErrorBoundary>
       <QueryClientProvider client={queryClient}>
         <App />
-        {process.env.NODE_ENV === "development" && <ReactQueryDevtools initialIsOpen={false} />}
       </QueryClientProvider>
     </ErrorBoundary>
   </React.StrictMode>
