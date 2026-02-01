7812b3685741b70258d7f5ed2c3b86cb94b2487d
jamesboder <james.macean08@gmail.com>
2026-01-31 22:12:23 -0500

    implement offline banner for offline mode

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -14,6 +14,7 @@ import { Toaster } from "react-hot-toast";
 import { VerseCardSkeleton } from "./components/common/Skeleton";
 import "./App.css";
 
+
 // Eager load critical authentication components
 import { Login } from "./features/auth/Login";
 import { Signup } from "./features/auth/Signup";
@@ -104,7 +105,7 @@ function App() {
                 path="/"
                 element={
                   <ProtectedRoute>
-                    <Layout />
+                    <Layout  />
                   </ProtectedRoute>
                 }
               >
--- frontend/src/components/layout/Layout.tsx
+++ frontend/src/components/layout/Layout.tsx
@@ -2,11 +2,14 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import { Header } from "./Header";
 import { Footer } from "./Footer";
+import { OfflineBanner } from "../common/OfflineBanner";
+
 
 export const Layout: React.FC = () => {
   return (
     <div className="min-h-screen flex flex-col">
       <Header />
+      <OfflineBanner />
       <main className="flex-1 py-8">
         <Outlet />
       </main>
