e5d1f081c1f06b2c625053b6c3c8c1b164bf70af
jamesboder <james.macean08@gmail.com>
2026-02-02 19:36:40 -0500

    Upload to fly.io, create fly.toml file, fix config issue-update serverAddress & Port

--- backend/internal/config/config.go
+++ backend/internal/config/config.go
@@ -37,12 +37,12 @@ func Load() (*Config, error) {
         DBPassword:      os.Getenv("DB_PASSWORD"),
         DBName:          os.Getenv("DB_NAME"),
         DBSSLMode:      os.Getenv("DB_SSLMODE"),
-        Port:            os.Getenv("PORT"),
+        Port:            getEnvOrDefault("PORT", "8080"),
         JWTSecret:       os.Getenv("JWT_SECRET"),
         BibleAPIKey:     os.Getenv("BIBLE_API_KEY"),
         BibleVersionID:  getEnvOrDefault("BIBLE_VERSION_ID", "de4e12af7f28f599-02"),
         BibleAPIBaseURL: getEnvOrDefault("BIBLE_API_BASE_URL", "https://rest.api.bible/v1"),
-        ServerAddress:   ":" + os.Getenv("PORT"),
+        ServerAddress:   "0.0.0.0:" + getEnvOrDefault("PORT", "8080"),
     }, nil
 }
 
